8a08d749df63984e812a4da65c95c781d0542b4e
defiantlyNotAlex <104746120+defiantlyNotAlex@users.noreply.github.com>
2022-10-13 15:55:44 +1030

    implemented damage calc

--- modules/Player.h
+++ modules/Player.h
@@ -7,7 +7,7 @@ class Player: public Sprite {
     // private:
 
     public:
-        Player(int x, int y, int lvl, int attack, int HPcapacity): Sprite(lvl, attack, HPcapacity) {
+        Player(int x, int y, int lvl, int attack, int defence, int HPcapacity): Sprite(lvl, attack, defence, 0, HPcapacity) {
             drawBody(x, y, sf::Color::Green);
         }
         ~Player() {}
