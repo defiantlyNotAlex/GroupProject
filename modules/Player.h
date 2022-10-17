228cd3726334495b26d70ce558ee363a2263c096
ZhouYHZ <110000082+ZhouYHZ@users.noreply.github.com>
2022-10-17 14:47:30 +1030

    Added player turn count function

--- modules/Player.h
+++ modules/Player.h
@@ -8,27 +8,28 @@
 class Player: public Character {
     private:
         sf::Text* text;
+        int num;
 
     public:
         // constructor with variables: x, y, lvl, atk, def, HP capacity
         Player(int x, int y, int lvl, int attack, int defence, int HPcapacity): Character(lvl, attack, defence, 0, HPcapacity) {
             drawBody(x, y, sf::Color::Green);
             hp->drawHPBar(x-10, y-15, sf::Color::Green);
-            printStats();
+            printStats(num);
             drawTypeText(x, y+20);
             hp->drawHPText(x+50, y-15);
         }
         // print player statistics
-        void printStats() {
+        void printStats(int num) {
             int* attributes = saveAtributes();
             text = new sf::Text();
             text->setFont(font);
             text->setCharacterSize(12);
             std::stringstream formatText;
-            formatText << "LVL: " << attributes[2] << "\nATK: " << attributes[3] << "\nDEF: " << attributes[4];
+            formatText << "LVL: " << attributes[2] << "\nATK: " << attributes[3] << "\nDEF: " << attributes[4] << "\nTURN: " << num;
             text->setString(formatText.str());
             text->setFillColor(sf::Color::Red);
-            text->setPosition(400,450);
+            text->setPosition(400,430);
         }
         // change player's statistic on screen
         void changePrintStats() {
@@ -48,6 +49,17 @@ class Player: public Character {
             win->draw(*text);
             win->draw(*typeText);
             hp->draw(win);
+        }
+
+        void playerturn(int num){
+            int* attributes = saveAtributes();
+
+            if (!font.loadFromFile("fonts/courbd.ttf")) {
+                std::cout << "Font not loading" << std::endl;
+            }
+            if (attributes[7] > 0){
+                printStats(num);
+            }
         }
         // destructor
         ~Player() {
